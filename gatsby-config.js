9b3ec7e33b7939b82763db8d95c8b4cac40cf987
erdkrai <erdkrai04@gmail.com>
2024-12-21 17:42:23 +0530

    completed till lesson 9

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'React Design Learning',
+    description: 'This course is for the learning purpose i.e learning the react for designers from scratch which will eventually help us to quickly do the live prototyping using the react framework',
+    keywords: 'react course, react for designers, prototyping, juspay'
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
