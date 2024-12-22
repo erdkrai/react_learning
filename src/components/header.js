9da8fdde0dd3850d3fe158d00d82f415c6584e5e
erdkrai <erdkrai04@gmail.com>
2024-12-23 01:28:04 +0530

    Completed chapter 12

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import './Header.css'
+import StripeCheckout from 'react-stripe-checkout'
 
 class Header extends React.Component {
   constructor(props) {
     super(props)
-    this.state={
-      hasScrolled:false
+
+    this.state = {
+      hasScrolled: false
     }
   }
 
@@ -14,30 +16,52 @@ class Header extends React.Component {
     window.addEventListener('scroll', this.handleScroll)
   }
 
-  handleScroll =(event)=>{
-    const scrollTop=window.pageYOffset
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
 
-    if (scrollTop>50){
-      this.setState({
-        hasScrolled:true
-      })
-    } else{
-      this.setState({
-        hasScrolled:false
-      })
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
     }
   }
-  render(){
-    return(
+
+  handlePurchase = (token) => {
+    const amount = 5000
+    const description = "My awesome product"
+
+    const bodyObject = {
+      tokenId: token.id,
+      email: token.email,
+      name: token.name,
+      description,
+      amount
+    }
+
+    fetch('http://localhost:9000/stripe-charge', {
+      method: 'POST',
+      body: JSON.stringify(bodyObject)
+    })
+  }
+
+  render() {
+    return (
       <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
-      <div className="HeaderGroup">
-        <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30"></img></Link>
-        <Link to="/courses">Courses</Link>
-        <Link to="/downloads">Downloads</Link>
-        <Link to="/workshops">Workshops</Link>
-        <Link to="/assignment"><button>View Assignment</button></Link>
+        <div className="HeaderGroup">
+          <Link to="/"><img src='/images/logo-designcode.svg' width="30" /></Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/downloads">Downloads</Link>
+          <Link to="/workshops">Workshops</Link>
+          <StripeCheckout
+            amount={ 5000 }
+            image="https://cl.ly/0K2f1V3K3h0D/download/Logo.jpg"
+            token={this.handlePurchase}
+            stripeKey={'pk_test_4VuxMZhOiYXJlElaTf3qjAXh'}
+            >
+            <button>Buy</button>
+          </StripeCheckout>
+        </div>
       </div>
-     </div>
     )
   }
 }
